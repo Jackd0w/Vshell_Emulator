5f6087b624aa1e80f6ae96aa3d58be75fa6a61f0
laughing man <komarovandrey396@gmail.com>
2021-10-29 00:39:50 +0300

    added cd command

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import sys
 import zipfile
 import operator
 
+try:
+    archieve_path = argv[1]
+    if not(zipfile.is_zipfile(archieve_path)):
+        raise Exception()
+except:
+    raise Exception('No archive name given')
+
+
+
+host_name = archieve_path.split('.')[0]
+current_dir = '/'
+
 def help():
     print('''
     List of available commands:
@@ -14,6 +26,28 @@ def help():
     ''')
 
 
+
+def cd(next_dir):
+    global current_dir
+    tmp_dir = ''
+
+    if next_dir == '..':
+        cur_list = current_dir.split('/')
+        cur_list = cur_list[1:len(cur_list) - 2]
+        tmp_dir = '/'.join(cur_list)
+        if tmp_dir == '':
+            tmp_dir = '/'
+        else:
+            tmp_dir = '/' + tmp_dir + '/'
+
+    else:
+        if ('/' in next_dir):
+            if (next_dir[-1] =='/'):
+                tmp_dir = next_dir
+            else:
+                tmp_dir = next_dir + '/'
+        else:
+            tmp_dir = current_dir + next_dir + '/'
 
 
 
